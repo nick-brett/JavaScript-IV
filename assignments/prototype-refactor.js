308b6646a6a80dbf0170cd82d885b3ec867f3079
Nicolas Brett <nicolasb59@gmail.com>
2019-06-13 21:46:27 -0400

    Reupload

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -150,6 +150,7 @@ const archer = new Humanoid({
 	language: 'Elvish'
 });
 
+
 console.log(mage.createdAt); // Today's date
 console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
 console.log(swordsman.healthPoints); // 15
